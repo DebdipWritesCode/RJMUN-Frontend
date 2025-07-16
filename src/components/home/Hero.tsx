28b59f3e5d31b27d9fd5af459bd0a981e9959171
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-07-16 15:17:26 +0530

    add hero section images and alignments

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,46 +1,46 @@
 const Hero = () => {
   return (
     <div className="relative w-full h-screen flex justify-center items-start pt-10">
-      {/* Flags image - hidden on small screens */}
-      <img
-        src="./images/Flags2.png"
-        className="
-          hidden sm:block
-          absolute
-          top-[60px] md:top-[80px]
-          left-[calc(50%-300px)] md:left-[calc(50%-600px)]
-          h-[400px] md:h-[500px] lg:h-[600px]
-          z-20
-          rotate-[-10deg]
-        "
-        alt="Flags"
-      />
-
-      {/* Main Box - fully responsive */}
+      {/* Main Box */}
       <div
         className="
           bg-[#9e9776]
-          w-[90%] sm:w-[500px] md:w-[600px] lg:w-[680px]
-          h-[300px] sm:h-[400px] md:h-[500px] lg:h-[560px]
+          w-[90%] max-w-[680px]
+          h-[500px]
           rounded-2xl shadow-2xl
           z-10
+          relative
         "
-      />
+      >
+        {/* Flags image - hide on small screens */}
+        <img
+          src="./images/Flags2.png"
+          className="
+            hidden md:block
+            absolute
+            top-[60px]
+            left-[-280px]
+            h-[550px]
+            z-20
+            rotate-[-10deg]
+          "
+          alt="Flags"
+        />
 
-      {/* Mic image - hidden on small screens */}
-      <img
-        src="./images/Mic.png"
-        className="
-          hidden sm:block
-          absolute
-          top-0
-          right-0
-          h-[400px] md:h-[500px] lg:h-[600px]
-          translate-x-[5%]
-          z-20
-        "
-        alt="Mic"
-      />
+        {/* Mic image - hide on small screens */}
+        <img
+          src="./images/Mic2.png"
+          className="
+            hidden md:block
+            absolute
+            top-[-200px]
+            right-[-300px]
+            h-[800px]
+            z-20
+          "
+          alt="Mic"
+        />
+      </div>
     </div>
   );
 };
